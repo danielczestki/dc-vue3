cd9a15f824abd0be698690cd969fb1185bf4e9ec
danielczestki <daniel@czestki.net>
2021-06-07 13:28:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dc-vue3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/dc-vue3)
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="name"/>
+    <br>
+    <br>
+    My name is <strong>{{ name }}</strong>
+  
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  
+  data() {
+    return {
+      name: ''
+    }
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
